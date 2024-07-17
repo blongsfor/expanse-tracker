8ac6e36a19839c6cb0e1dc99a3bc4b8ddfd8ecc6
Björn <bjoern.n@mailbox.org>
2024-07-17 16:16:54 +0200

    add HomePage and EntryForm Component

---
+++ src/components/EntryForm.jsx
@@ -0,0 +1,10 @@
+export default function EntryForm() {
+  return (
+    <>
+      <form>
+        <label for="expense">Enter your Expense here</label>
+        <input type="text" id="expense" name="expense" />
+      </form>
+    </>
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,10 @@
-export default function Home() {
+import EntryForm from "@/components/EntryForm";
+
+export default function HomePage() {
   return (
     <>
       <h1>Expense Tracker</h1>
+      <EntryForm />
     </>
   );
 }
