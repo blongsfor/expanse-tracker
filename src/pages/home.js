fa070c66f40ed977e069694b2483c75290f0a527
Björn <bjoern.n@mailbox.org>
2024-07-17 16:28:51 +0200

    add buttons to HomePage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,14 @@
 import EntryForm from "@/components/EntryForm";
+import MinusButton from "@/components/MinusButton";
+import PlusButton from "@/components/PlusButton";
 
 export default function HomePage() {
   return (
     <>
       <h1>Expense Tracker</h1>
       <EntryForm />
+      <PlusButton />
+      <MinusButton />
     </>
   );
 }
