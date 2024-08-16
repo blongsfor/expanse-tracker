cc533a62f1c52c7794ccfeff2ca56e57a5664f4a
Bjoern <bjoern.n@mailbox.org>
2024-08-16 13:59:10 +0200

    add POST request to api/expenses

--- src/pages/api/expenses/expenses.js
+++ src/pages/api/expenses/expenses.js
@@ -4,7 +4,7 @@ import Expense from "../../../../lib/models/expenses";
 export default async function handler(req, res) {
   await dbConnect();
 
-  try {
+  /*   try {
     if (req.method === "GET") {
       const expenses = await Expense.find().lean();
       return res.status(200).json(expenses);
@@ -12,6 +12,24 @@ export default async function handler(req, res) {
       return res
         .status(405)
         .json({ error: `Method ${req.method} not allowed` });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ error: "Server error" });
+  } 
+} */
+
+  try {
+    switch (req.method) {
+      case "GET":
+        const expenses = await Expense.find().lean();
+        return res.status(200).json(expenses);
+
+      case "POST":
+        const { name, from, value, date } = req.body;
+        const newExpense = new Expenses({ name, from, value, date });
+        await newExpense.save();
+        return res.status(201).json({ status: "Expense saved successfully" });
     }
   } catch (error) {
     console.error("Error:", error);
