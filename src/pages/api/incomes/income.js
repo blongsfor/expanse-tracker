6697d5895c29e89a3fe4b5fa87cdfc4c075c4d68
Bjoern <bjoern.n@mailbox.org>
2024-08-21 13:24:35 +0200

    add POST method to /api/income.js and update model for income

--- src/pages/api/incomes/income.js
+++ src/pages/api/incomes/income.js
@@ -13,6 +13,20 @@ export default async function handler(req, res) {
         return res
           .status(405)
           .json({ error: `Method ${req.method} not allowed` });
+
+      case "POST":
+        const { entrytype, category, amount, date, description, notes } =
+          req.body;
+        const newIncome = new Income({
+          entrytype,
+          category,
+          amount,
+          date,
+          description,
+          notes,
+        });
+        const savedIncome = await newIncome.save();
+        return res.status(201) - json(savedIncome);
     }
   } catch (error) {
     // Keep this log for error tracking
