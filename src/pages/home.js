24365e3bf4ce6965e24fee7a199147fd1a934a27
Björn <bjoern.n@mailbox.org>
2024-09-18 15:03:39 +0200

    rename to LoginButton.jsx add callbacks to nextauth.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import EntryForm from "@/components/EntryForm";
+import Component from "@/components/LoginButton";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
     <>
+      <Component />
       <h1>Expense Tracker</h1>
       <EntryForm />
       <Link href={`/expenselist`}>Go to Expense List</Link>
